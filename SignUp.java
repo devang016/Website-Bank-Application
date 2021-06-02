17c5cd99541e77c7f3ebb55ab7fd76e5b862eaec
vanshika1203 <78428195+vanshika1203@users.noreply.github.com>
2021-06-02 22:35:30 +0530

    Create SignUp.java

---
+++ SignUp.java
@@ -0,0 +1,58 @@
+package com.Banking;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class SignUp
+ */
+@WebServlet("/Sign Up (New Customers)")
+public class SignUp extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignUp() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		
+		
+		String fname = request.getParameter("fname");
+		String lname = request.getParameter("lname");
+		String phone = request.getParameter("phone");
+		String email = request.getParameter("email");
+		String address  = request.getParameter("address");
+		
+		Recentcustomer recentcustomer = new Recentcustomer(fname , lname , phone , email , address);
+		
+		
+		
+        Signupdao sdao = new Signupdao();
+        String result = sdao.insert(recentcustomer);
+        response.getWriter().print(result);
+	}
+
+}
