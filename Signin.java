84250085163f87d0a309c597eee1d658fbaca3a5
vanshika1203 <78428195+vanshika1203@users.noreply.github.com>
2021-06-02 23:41:57 +0530

    Create Signin.java

---
+++ Signin.java
@@ -0,0 +1,54 @@
+package com.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.Banking.Signupdao;
+
+/**
+ * Servlet implementation class SignIn
+ */
+@WebServlet("/Sign In (Existing Customers)")
+public class SignIn extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignIn() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		String  id = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		Existingcustomer existingcustomer = new Existingcustomer(id , password);
+		
+		 Signindao signindao = new Signindao();
+		String result = signindao.insert (existingcustomer);
+		 response.getWriter().print(result);
+		
+	}
+	
+
+}
